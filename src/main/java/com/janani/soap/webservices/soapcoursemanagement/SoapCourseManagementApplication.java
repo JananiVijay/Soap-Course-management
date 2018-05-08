da6aaeb52161ed4789a0c264f3ada5dcc9e2661b
jananivijay <jananilakshmi.87@gmail.com>
2018-05-08 15:17:46 -0400

    Added a comment!

--- src/main/java/com/janani/soap/webservices/soapcoursemanagement/SoapCourseManagementApplication.java
+++ src/main/java/com/janani/soap/webservices/soapcoursemanagement/SoapCourseManagementApplication.java
@@ -8,5 +8,6 @@ public class SoapCourseManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoapCourseManagementApplication.class, args);
+		//added all the files to github
 	}
 }
