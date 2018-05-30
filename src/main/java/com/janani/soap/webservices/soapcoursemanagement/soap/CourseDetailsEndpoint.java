14c3a7f2255e57cc00546eec300e583f73f526b7
jananivijay <jananivijay@NCI-02069124.nci.nih.gov>
2018-05-30 11:54:32 -0400

    Added getAllCourseDetails, deleteCourseDetails - deleteById

--- src/main/java/com/janani/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
+++ src/main/java/com/janani/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -1,5 +1,7 @@
 package com.janani.soap.webservices.soapcoursemanagement.soap;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -7,6 +9,10 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import com.janani.courses.CourseDetails;
+import com.janani.courses.DeleteCourseDetailsRequest;
+import com.janani.courses.DeleteCourseDetailsResponse;
+import com.janani.courses.GetAllCourseDetailsRequest;
+import com.janani.courses.GetAllCourseDetailsResponse;
 import com.janani.courses.GetCourseDetailsRequest;
 import com.janani.courses.GetCourseDetailsResponse;
 import com.janani.soap.webservices.soapcoursemanagement.soap.bean.Course;
@@ -26,22 +32,52 @@ public class CourseDetailsEndpoint {
 	//name - GetCourseDetailsRequest
 	@PayloadRoot(namespace="http://janani.com/courses", localPart="GetCourseDetailsRequest")
 	@ResponsePayload
-	public GetCourseDetailsResponse processGetCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
-		
-		Course course = service.findById(request.getId());
-		
-		return mapCourse(course);
+	public GetCourseDetailsResponse processGetCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {		
+		Course course = service.findById(request.getId());		
+		return mapCourseDetails(course);
 	}
-	private GetCourseDetailsResponse mapCourse(Course course) {
-		GetCourseDetailsResponse response = new GetCourseDetailsResponse();	
-		
+
+	private GetCourseDetailsResponse mapCourseDetails(Course course) {
+		GetCourseDetailsResponse response = new GetCourseDetailsResponse();		
+		response.setCourseDetails(mapCourse(course));
+		return response;
+	}
+
+	private GetAllCourseDetailsResponse mapAllCourseDetails(List<Course> courses) {
+		GetAllCourseDetailsResponse response = new GetAllCourseDetailsResponse();	
+		for(Course course: courses) {
+			CourseDetails mapCourse = mapCourse(course);
+			response.getCourseDetails().add(mapCourse);
+		}
+		return response;
+	}
+	
+	private CourseDetails mapCourse(Course course) {
 		CourseDetails courseDetails = new CourseDetails();
 		
 		courseDetails.setId(course.getId());
 		courseDetails.setName(course.getName());
 		courseDetails.setDescription(course.getDescription());
+		return courseDetails;
+	}
+	
+	@PayloadRoot(namespace="http://janani.com/courses", localPart="GetAllCourseDetailsRequest")
+	@ResponsePayload
+	public GetAllCourseDetailsResponse processAllGetCourseDetailsRequest(@RequestPayload GetAllCourseDetailsRequest request) {
 		
-		response.setCourseDetails(courseDetails);
+		List<Course> courses = service.findAll();
+		
+		return mapAllCourseDetails(courses);
+	}
+	
+	@PayloadRoot(namespace="http://janani.com/courses", localPart="DeleteCourseDetailsRequest")
+	@ResponsePayload
+	public DeleteCourseDetailsResponse deleteCourseDetailsRequest(@RequestPayload DeleteCourseDetailsRequest request) {
+		
+		int status = service.deleteById(request.getId());
+		
+		DeleteCourseDetailsResponse response = new DeleteCourseDetailsResponse();
+		response.setStatus(status);
 		return response;
 	}
 
