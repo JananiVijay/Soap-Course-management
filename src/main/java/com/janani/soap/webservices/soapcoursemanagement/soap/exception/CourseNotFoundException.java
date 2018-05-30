9757767cea246c594840a8dfec594abaa60f3d89
jananivijay <jananivijay@NCI-02069124.nci.nih.gov>
2018-05-30 15:52:08 -0400

    Added custom courseNotFoundException with custom faultCode

---
+++ src/main/java/com/janani/soap/webservices/soapcoursemanagement/soap/exception/CourseNotFoundException.java
@@ -0,0 +1,16 @@
+package com.janani.soap.webservices.soapcoursemanagement.soap.exception;
+
+import org.springframework.ws.soap.server.endpoint.annotation.FaultCode;
+import org.springframework.ws.soap.server.endpoint.annotation.SoapFault;
+
+@SoapFault(faultCode = FaultCode.CUSTOM, customFaultCode = "{http://janani.com/courses}001_COURSE_NOT_FOUND" )
+public class CourseNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = -6336372906044469939L;
+
+	public CourseNotFoundException(String message) {
+		super(message);
+	}
+	
+
+}
